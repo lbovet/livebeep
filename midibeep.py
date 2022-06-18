2a3372cbc12e56f2ca829438ecfe901af32cd224
Laurent Bovet <laurent.bovet@windmaster.ch>
2022-06-19 00:03:16 +0200

    feat: minimal midi control

--- midibeep.py
+++ midibeep.py
@@ -1,17 +1,26 @@
 from mididings import *
 
-c = 0
+channel_controls = dict()
 
-def control_state(ev):
-    if ev.type == CTRL:
-        global c
-        c = c + (64.0 - ev.data2) / 64.0
+def set_control(channel, control, value):
+    global channel_controls
+    controls = channel_controls.setdefault(channel, dict())
+    controls[control] = value
+
+def get_control(channel, control):
+    global channel_controls
+    return channel_controls.get(channel, dict()).get(control, 64)
+
+def handle_control(ev):
+    if ev.type == CTRL and ev.ctrl >= 60 and ev.ctrl <= 67:
+        c = get_control(ev.channel, ev.ctrl)
+        c = c + (64.0 - ev.value) / 64.0
         if c < 0:
             c = 0
         if c > 127:
             c = 127
-        ev.data2 = int(c)
+        ev.value = int(c)
+        set_control(ev.channel, ev.ctrl, c)
     return ev
 
-
-run(Process(control_state))
+run(Process(handle_control) >> Velocity(gamma=3))
